b939ce34d7fa7b0c15d6e091af478ba2d5a739f5
Kiraogi <golokron3@yandex.ru>
2025-02-05 21:48:28 +0300

    ver 0.01.04
    Fix typo in Post model: change 'bode' to 'body'

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='myblog_posts')
-    bode = models.TextField()
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True) 
