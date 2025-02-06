bda5194752a373a0a265fe352d3b89928661434e
Kiraogi <golokron3@yandex.ru>
2025-02-06 21:31:05 +0300

    ver 0.01.06
    Add post_detail view and import get_object_or_404

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
 from .models import Post
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED)                   
+    return render(request, 'myblog/post/detail.html', {'post': post})
+
 
 def post_list(request):
     posts = Post.published.all()
