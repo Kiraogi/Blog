584ab9e81a8019b8fd8e3fbe5bb33bb497718361
Kiraogi <golokron3@yandex.ru>
2025-02-09 20:08:03 +0300

    ver 0.01.09
    Add unique slug for date and update URL structure

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -3,8 +3,12 @@ from django.http import Http404
 
 from .models import Post
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED)                   
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status = Post.Status.PUBLISHED, 
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)                   
     return render(request, 'myblog/post/detail.html', {'post': post})
 
 
