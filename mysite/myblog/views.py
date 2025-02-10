e2c7ff60ff4f1403214427b89579593e1c7fa727
Kiraogi <golokron3@yandex.ru>
2025-02-10 19:18:54 +0300

    ver 0.01.11
    Refactor post list view to use ListView and fix pagination

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
+
+def post_list(request):
+    post_list = Post.published.all()
+    #Построчная разбивка с 5 постами на страницу
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        #Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        #Если page_number находится вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'myblog/post/list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status = Post.Status.PUBLISHED, 
@@ -11,11 +28,11 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)                   
     return render(request, 'myblog/post/detail.html', {'post': post})
 
-
-def post_list(request):
-    post_list = Post.published.all()
-    #Построчная разбивка с 5 постами на страницу
-    paginator = Paginator(post_list, 5)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request, 'myblog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name='posts'
+    paginate_by = 5
+    template_name = 'myblog/post/list.html'
