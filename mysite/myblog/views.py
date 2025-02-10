558b466b9c340c225f093ebd9d2a1bc230ff54b4
Kiraogi <golokron3@yandex.ru>
2025-02-10 21:11:44 +0300

    ver 0.01.12
    Add share feature to post detail and configure email backend

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 from .models import Post
+from .forms import EmailPostForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -36,3 +38,24 @@ class PostListView(ListView):
     context_object_name='posts'
     paginate_by = 5
     template_name = 'myblog/post/list.html'
+
+def post_share(request, post_id):
+    #Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
+    if request.method == 'POST':
+        #Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'myblog/post/share.html', {'post': post, 'form': form, 'sent': sent})
